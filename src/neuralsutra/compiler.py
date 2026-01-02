67611e48ad85a3aa5c71602e2c37e7ab2005e6ff
SaiNikhilTadepalli <saitadepalli2006@gmail.com>
2026-01-02 11:02:07 +0000

    Load vocab and trained model from path in constructor method

--- src/neuralsutra/compiler.py
+++ src/neuralsutra/compiler.py
@@ -1,6 +1,17 @@
+import torch
+
+from neuralsutra.router import Router
+from neuralsutra.vocab import load_vocab
+
+
 class Compiler:
-    def __init__(self):
-        pass
+    def __init__(self, model_path, vocab_path):
+        self.vocab = load_vocab(vocab_path)
+        self.model = Router(vocab_size=len(self.vocab) + 1)
+
+        # Load the trained model (.pth) file
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
 
     def compile(self, expr, var):
         pass
