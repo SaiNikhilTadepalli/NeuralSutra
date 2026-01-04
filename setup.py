13a02f7605d21ddb0ca554461918c66669d949db
SaiNikhilTadepalli <saitadepalli2006@gmail.com>
2026-01-04 12:27:49 +0000

    Link README.md and add project URLs

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of the README file for the long description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="neuralsutra",
@@ -12,7 +17,20 @@ setup(
         "scikit-learn",
         "pytest",
     ],
-    description="A Neuro-Symbolic Vedic Integration Engine",
+    # Metadata for recruiters/users
+    description="A Hybrid Neuro-Symbolic Integration Engine using Vedic Mathematics Kernels",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Sai Nikhil Tadepalli",
+    url="https://github.com/SaiNikhilTadepalli/NeuralSutra",
+    project_urls={
+        "Bug Tracker": "https://github.com/SaiNikhilTadepalli/NeuralSutra/issues",
+        "Documentation": "https://github.com/SaiNikhilTadepalli/NeuralSutra#readme",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
     python_requires=">=3.11",
 )
